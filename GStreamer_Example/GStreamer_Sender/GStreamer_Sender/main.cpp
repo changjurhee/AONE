c13e89a057db838850e59601f72bde98d7a1b3fd
Jiung Choi(최지웅) <ju.choi@lginnotek.com>
2023-06-27 14:12:58 +0900

    [ENV] Add audio transmission/reception function
    [Desc]
    - Add audio transmission/reception function

--- GStreamer_Example/GStreamer_Sender/GStreamer_Sender/main.cpp
+++ GStreamer_Example/GStreamer_Sender/GStreamer_Sender/main.cpp
@@ -1,81 +1,103 @@
-#include <gst/gst.h>
+ï»¿#include <gst/gst.h>
 
-int main(int argc, char* argv[]) {
-    // GStreamer ÃÊ±âÈ­
+int main(int argc, char* argv[]) 
+{
+    // Initialize GStreamer
     gst_init(&argc, &argv);
 
-    // GStreamer ÆÄÀÌÇÁ¶óÀÎ »ý¼º
-    GstElement* pipeline = gst_pipeline_new("sender_pipeline");
-    GstElement* src = gst_element_factory_make("mfvideosrc", "src");
-    GstElement* enc = gst_element_factory_make("x264enc", "enc");
-    GstElement* pay = gst_element_factory_make("rtph264pay", "pay");
-    GstElement* capsfilter = gst_element_factory_make("capsfilter", "filter");
-    GstElement* sink = gst_element_factory_make("udpsink", "sink");
+    // Create GStreamer pipline
+    GstElement* pipeline = gst_pipeline_new("pipeline");
 
-    // ÆÄÀÌÇÁ¶óÀÎ¿¡ ¿ä¼Ò Ãß°¡
-    gst_bin_add_many(GST_BIN(pipeline), src, enc, pay, capsfilter, sink, NULL);
+    // Create GStreamer pipline elements for video
+    GstElement* videoSrc = gst_element_factory_make("mfvideosrc", "videoSrc");
+    GstElement* videoEnc = gst_element_factory_make("x264enc", "videoEnc");
+    GstElement* videoPay = gst_element_factory_make("rtph264pay", "videoPay");
+    GstElement* videoSink = gst_element_factory_make("udpsink", "videoSink");
 
-    // ¿ä¼Ò °£ ¸µÅ© ¼³Á¤
-    gst_element_link_many(src, enc, pay, capsfilter, sink, NULL); 
+    // Create GStreamer pipline elements for audio
+    GstElement* audioSrc = gst_element_factory_make("autoaudiosrc", "audioSrc");
+    GstElement* audioConv = gst_element_factory_make("audioconvert", "audioConv");
+    GstElement* audioResample = gst_element_factory_make("audioresample", "audioResample");
+    GstElement* audioOpusenc = gst_element_factory_make("opusenc", "audioOpusenc");
+    GstElement* audioPay = gst_element_factory_make("rtpopuspay", "audioPay");
+    GstElement* audioSink = gst_element_factory_make("udpsink", "audioSink");
 
-    // ¼Û½Å ÁÖ¼Ò ¼³Á¤ (IP ÁÖ¼Ò´Â 127.0.0.1·Î °íÁ¤, Æ÷Æ® ¹øÈ£ 5001)
-    g_object_set(sink, "host", "127.0.0.1", "port", 5001, NULL);
+    // Add element to pipeline
+    gst_bin_add_many(GST_BIN(pipeline), videoSrc, videoEnc, videoPay, videoSink, audioSrc, audioConv, audioResample, audioOpusenc, audioPay, audioSink, NULL);
 
-    // RTP Çü½Ä(caps) ¼³Á¤
-    GstCaps* caps = gst_caps_from_string("application/x-rtp, media=(string)video, payload=(int)96");
-    g_object_set(G_OBJECT(capsfilter), "caps", caps, NULL);
-    gst_caps_unref(caps);
+    // linking elements for video
+    gst_element_link_many(videoSrc, videoEnc, videoPay, videoSink, NULL);
 
-    // ³ëÆ®ºÏ Ä«¸Þ¶ó ¼³Á¤
-    g_object_set(src, "device-index", 0, NULL);  // 0Àº Ã¹ ¹øÂ° Ä«¸Þ¶ó¸¦ ³ªÅ¸³À´Ï´Ù.
+    // linking elements for audio
+    gst_element_link_many(audioSrc, audioConv, audioResample, audioOpusenc, audioPay, audioSink, NULL);
 
-    // x264enc ¿ä¼ÒÀÇ tune ¼Ó¼ºÀ» "zerolatency"·Î ¼³Á¤
-    g_object_set(enc, "tune", 0x00000004, NULL);
+    // Set the receiving IP and port for video
+    g_object_set(videoSink, "host", "127.0.0.1", "port", 5001, NULL);
 
-    // ÆÄÀÌÇÁ¶óÀÎ ½ÇÇà
+    // Set the receiving IP and port for audio
+    g_object_set(audioSink, "host", "127.0.0.1", "port", 5002, NULL);
+
+    // set up laptop camera
+    g_object_set(videoSrc, "device-index", 0, NULL);  // 0ì€ ì²« ë²ˆì§¸ ì¹´ë©”ë¼ë¥¼ ë‚˜íƒ€ëƒ…ë‹ˆë‹¤.
+
+    // Set the tune attribute of the x264enc element to "zerolatency"
+    g_object_set(videoEnc, "tune", 0x00000004, NULL);
+
+    // generic (2049)               â€“ Generic audio
+    // voice (2048)                 â€“ Voice
+    // restricted-lowdelay (2051)   â€“ Restricted low delay
+    // Set the opusenc element's audio-type attribute to "restricted-lowdelay"
+    g_object_set(audioOpusenc, "audio-type", 2051, NULL);
+
+    // Pipeline execution
     GstStateChangeReturn ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
-    if (ret == GST_STATE_CHANGE_FAILURE) {
+    if (ret == GST_STATE_CHANGE_FAILURE)
+    {
         g_printerr("Unable to start the pipeline.\n");
         gst_object_unref(pipeline);
         return -1;
     }
 
-    // ¸ÞÀÎ ·çÇÁ ½ÇÇà
+    // Run main loop
     GstBus* bus = gst_element_get_bus(pipeline);
     GstMessage* msg;
-    while ((msg = gst_bus_poll(bus, GST_MESSAGE_ANY, GST_CLOCK_TIME_NONE))) {
+    while ((msg = gst_bus_poll(bus, GST_MESSAGE_ANY, GST_CLOCK_TIME_NONE)))
+    {
         GError* err;
         gchar* debug_info;
-        switch (GST_MESSAGE_TYPE(msg)) {
-        case GST_MESSAGE_ERROR:
-            gst_message_parse_error(msg, &err, &debug_info);
-            g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
-            g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
-            g_error_free(err);
-            g_free(debug_info);
-            break;
-        case GST_MESSAGE_EOS:
-            g_print("End-Of-Stream reached.\n");
-            break;
-        case GST_MESSAGE_STATE_CHANGED: {
-            GstState old_state, new_state, pending_state;
-            gst_message_parse_state_changed(msg, &old_state, &new_state, &pending_state);
-            g_print("State changed from %s to %s\n", gst_element_state_get_name(old_state), gst_element_state_get_name(new_state));
-            break;
-        }
-        case GST_MESSAGE_BUFFERING: {
-            gint percent = 0;
-            gst_message_parse_buffering(msg, &percent);
-            g_print("Buffering %d%%\n", percent);
-            break;
-        }
-        default:
-            break;
+        switch (GST_MESSAGE_TYPE(msg)) 
+        {
+            case GST_MESSAGE_ERROR:
+                gst_message_parse_error(msg, &err, &debug_info);
+                g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
+                g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
+                g_error_free(err);
+                g_free(debug_info);
+                break;
+            case GST_MESSAGE_EOS:
+                g_print("End-Of-Stream reached.\n");
+                break;
+            case GST_MESSAGE_STATE_CHANGED: 
+            {
+                GstState old_state, new_state, pending_state;
+                gst_message_parse_state_changed(msg, &old_state, &new_state, &pending_state);
+                g_print("State changed from %s to %s\n", gst_element_state_get_name(old_state), gst_element_state_get_name(new_state));
+                break;
+            }
+            case GST_MESSAGE_BUFFERING:
+            {
+                gint percent = 0;
+                gst_message_parse_buffering(msg, &percent);
+                g_print("Buffering %d%%\n", percent);
+                break;
+            }
+            default:
+                break;
         }
         gst_message_unref(msg);
     }
 
-    // ÆÄÀÌÇÁ¶óÀÎ Á¤Áö ¹× ÇØÁ¦
+    // Release the pipeline
     gst_element_set_state(pipeline, GST_STATE_NULL);
     gst_object_unref(pipeline);
     return 0;
