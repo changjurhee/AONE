cf0ab453c0b50ac5dbe453fec0fc2639e80a9fb3
changju.rhee <changju.rhee@lge.com>
2023-07-05 10:50:04 +0900

    1) OnVadNotify Window User Message 방식 샘플 추가 - 참고

--- src/VoIPClient/CallView.cpp
+++ src/VoIPClient/CallView.cpp
@@ -26,6 +26,7 @@ void CCallView::DoDataExchange(CDataExchange* pDX)
 }
 
 BEGIN_MESSAGE_MAP(CCallView, CFormView)
+	ON_MESSAGE(UM_VAD_STATE, &CCallView::OnVadNotify)
 END_MESSAGE_MAP()
 
 
@@ -47,3 +48,37 @@ void CCallView::Dump(CDumpContext& dc) const
 
 
 // CCallView 메시지 처리기
+
+LRESULT CCallView::OnVadNotify(WPARAM wParam, LPARAM lParam)
+{
+    // @todo 아래 코드는 좀 수정이 필요 함. 
+   /* HWND hTempWnd;
+    litevad_result_t VadState;
+    hTempWnd = GetDlgItem(hWnd, IDC_VAD_STATE_STATUS);
+    VadState = (litevad_result_t)wParam;
+    switch (VadState)
+    {
+    case LITEVAD_RESULT_SPEECH_BEGIN:
+        SetWindowTextA(hTempWnd, "Speech");
+        break;
+    case LITEVAD_RESULT_SPEECH_END:
+        SetWindowTextA(hTempWnd, "Speech End");
+        break;
+    case LITEVAD_RESULT_SPEECH_BEGIN_AND_END:
+        SetWindowTextA(hTempWnd, "Speech Begin & End");
+        break;
+    case LITEVAD_RESULT_FRAME_SILENCE:
+        SetWindowTextA(hTempWnd, "Silence");
+        break;
+    case LITEVAD_RESULT_FRAME_ACTIVE:
+        break;
+    case LITEVAD_RESULT_ERROR:
+        SetWindowTextA(hTempWnd, "VAD Error");
+        break;
+    default:
+        SetWindowTextA(hTempWnd, "Unknown Error");
+        break;
+    }*/
+
+	return TRUE;
+}
