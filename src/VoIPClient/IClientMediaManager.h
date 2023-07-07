e6c9e0baa3f38b0659be13027ca5c3a410850418
doosan.baek <doosan.baek@lge.com>
2023-07-07 13:02:18 +0900

    Modify interface for session

--- src/VoIPClient/IClientMediaManager.h
+++ src/VoIPClient/IClientMediaManager.h
@@ -3,7 +3,7 @@
 #include "../json/json.h"
 
 struct IClientMediaManager {
-	virtual void registerNotifyVideoQualityCallback(void (*notifyVideoQuality)(int)) = 0;
+	virtual void registerRequestVideoQualityCallback(void (*requestVideoQuality)(int)) = 0;
 	virtual void setVideoQuality(int video_quality_index) = 0;
 	virtual void startCall(Json::Value client_info) = 0;
 	virtual void endCall(Json::Value client_info) = 0;
