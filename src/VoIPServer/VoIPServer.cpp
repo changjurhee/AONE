86b2d953fe48dda87f118b74ca6426b8ca130c29
junsoo81.park <junsoo81.park@lge.com>
2023-07-12 02:10:22 +0900

    Fix log to debug, video 'sync'=true, MEDIA_ENABLED

--- src/VoIPServer/VoIPServer.cpp
+++ src/VoIPServer/VoIPServer.cpp
@@ -12,6 +12,7 @@
 #include "VoIPServerDoc.h"
 #include "VoIPServerView.h"
 #include "ServerMediaManager.h"
+#include "common/logger.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -79,6 +80,8 @@ const WORD _wVerMinor = 0;
 
 BOOL CVoIPServerApp::InitInstance()
 {
+	Logger::GetInstance()->SetLogLevel(LogLevel::LL_DEBUG);
+
 	// 애플리케이션 매니페스트가 ComCtl32.dll 버전 6 이상을 사용하여 비주얼 스타일을
 	// 사용하도록 지정하는 경우, Windows XP 상에서 반드시 InitCommonControlsEx()가 필요합니다. 
 	// InitCommonControlsEx()를 사용하지 않으면 창을 만들 수 없습니다.
